80b1ec09beabdfed34784e46be60509b7ba6b014
Pierre Molinaro <pierre@pcmolinaro.name>
2011-07-16 09:00:38 +0000

    Ajout fichiers oubliés

---
+++ sample-code/simple-document/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  simple-document
+//
+//  Created by Pierre Molinaro on 15/07/11.
+//  Copyright __MyCompanyName__ 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
