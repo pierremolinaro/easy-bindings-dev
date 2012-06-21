0d0ee93fd36ce4c3082e7b79bddecbebe127f43a
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2012-06-21 14:48:51 +0000

    Refonte des exemples

---
+++ sample-projects/01-simple-preferences/01-simple-preferences/PMAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  PMAppDelegate.m
+//  01-simple-preferences
+//
+//  Created by Pierre Molinaro on 21/06/12.
+//  Copyright (c) 2012 ECN / IRCCyN. All rights reserved.
+//
+
+#import "PMAppDelegate.h"
+
+@implementation PMAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
