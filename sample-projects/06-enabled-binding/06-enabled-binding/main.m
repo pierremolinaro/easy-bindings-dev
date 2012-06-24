c80047003a55a48f14b486170fb0e9915f17345c
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2012-06-24 12:30:04 +0000

    Début $enabled binding (exemple 06)

---
+++ sample-projects/06-enabled-binding/06-enabled-binding/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  06-enabled-binding
+//
+//  Created by Pierre Molinaro on 24/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
