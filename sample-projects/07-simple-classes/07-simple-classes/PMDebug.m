4d4b572efdeaad49695368148f9b843cbdba32a6
Pierre Molinaro <pierre@pcmolinaro.name>
2012-11-13 18:26:01 +0000

    Ajout exemple 07 — simples classes

---
+++ sample-projects/07-simple-classes/07-simple-classes/PMDebug.m
@@ -0,0 +1,262 @@
+//----------------------------------------------------------------------------*
+
+#import "PMDebug.h"
+#import "easy-bindings-utilities.h"
+
+//----------------------------------------------------------------------------*
+
+static PMDebug * gDebugObject ;
+
+//----------------------------------------------------------------------------*
+
+void macroNoteObjectAllocation (NSObject * inObject) {
+  if (nil == gDebugObject) {
+    gDebugObject = [PMDebug new] ;
+    const BOOL ok = [NSBundle loadNibNamed:@"PMDebug" owner:gDebugObject] ;
+    if (! ok) {
+      presentErrorWindow (__FILE__, __LINE__, @"Cannot load 'PMDebug' nib file") ;
+    }
+  }
+  [gDebugObject pmNoteObjectAllocation:inObject] ;
+}
+
+//----------------------------------------------------------------------------*
+
+void macroNoteObjectDeallocation (NSObject * inObject) {
+  [gDebugObject pmNoteObjectDeallocation:inObject] ;
+}
+
+//----------------------------------------------------------------------------*
+
+
+@implementation PMDebug
+
+//----------------------------------------------------------------------------*
+
+@synthesize mAllocationStatsWindowVisibleAtLaunch ;
+@synthesize mAllocatedObjectCount ;
+@synthesize mTotalAllocatedObjectCount ;
+@synthesize mDisplayFilter ;
+
+//----------------------------------------------------------------------------*
+//    pmInstallDebugMenu                                                      *
+//----------------------------------------------------------------------------*
+
+- (void) pmInstallDebugMenu {
+  NSMenuItem * item = [[NSMenuItem alloc]
+    initWithTitle:@"Schmutrz"
+    action:NULL
+    keyEquivalent:@""
+  ] ;
+  [item setSubmenu:mDebugMenu] ;
+  [[NSApp mainMenu] addItem:item] ;
+}
+
+//----------------------------------------------------------------------------*
+//    init                                                                    *
+//----------------------------------------------------------------------------*
+
+- (id) init {
+  //  NSLog (@"%s %p", __PRETTY_FUNCTION__, self) ;
+  self = [super init] ;
+  if (self) {
+    mAllocatedObjectCountByClass = [NSCountedSet new] ;
+    mTotalAllocatedObjectCountByClass = [NSCountedSet new] ;
+  //---
+    [[NSNotificationCenter defaultCenter]
+      addObserver:self
+      selector:@selector(applicationWillTerminateAction:)
+      name:NSApplicationWillTerminateNotification
+      object:nil
+    ] ;
+  //---
+    if (nil != [NSApp mainMenu]) {
+      [self pmInstallDebugMenu] ;
+    }else{
+      [[NSNotificationCenter defaultCenter]
+        addObserver:self
+        selector:@selector(applicationDidFinishLaunching:)
+        name:NSApplicationDidFinishLaunchingNotification
+        object:nil
+      ] ;
+    }
+  }
+  return self ;
+}
+
+//----------------------------------------------------------------------------*
+//    applicationDidFinishLaunching:                                          *
+//----------------------------------------------------------------------------*
+
+- (void) applicationDidFinishLaunching: (NSNotification *) inNotification {
+  [self pmInstallDebugMenu] ;
+}
+
+//----------------------------------------------------------------------------*
+//    awakeFromNib                                                            *
+//----------------------------------------------------------------------------*
+
+- (void) awakeFromNib {
+  // NSLog (@"%s %p %p", __PRETTY_FUNCTION__, self, mDebugMenu) ;
+//--- Allocation Window visibility
+  self.mAllocationStatsWindowVisibleAtLaunch = [[NSUserDefaults standardUserDefaults]
+    boolForKey:@"PMDebug:allocationStatsWindowVisible"
+  ] ;
+  self.mDisplayFilter = [[NSUserDefaults standardUserDefaults]
+    integerForKey:@"PMDebug:allocationStatsDisplayFilter"
+  ] ;
+//--- Allocation stats window visibility at Launch
+  if (self.mAllocationStatsWindowVisibleAtLaunch) {
+    [mAllocationStatsWindow makeKeyAndOrderFront:nil] ;
+  }
+//--- Bindings
+  [mAllocationStatsWindowVisibleAtLaunchCheckbox
+    bind:@"value"
+    toObject:self
+    withKeyPath:@"mAllocationStatsWindowVisibleAtLaunch"
+    options:nil
+  ] ;
+  [mCurrentlyAllocatedObjectCountTextField
+    bind:@"value"
+    toObject:self
+    withKeyPath:@"mAllocatedObjectCount"
+    options:nil
+  ] ;
+  [mTotalAllocatedObjectCountTextField
+    bind:@"value"
+    toObject:self
+    withKeyPath:@"mTotalAllocatedObjectCount"
+    options:nil
+  ] ;
+  [mDisplayFilterPopUpButton
+    bind:@"selectedIndex"
+    toObject:self
+    withKeyPath:@"mDisplayFilter"
+    options:nil
+  ] ;
+}
+
+//----------------------------------------------------------------------------*
+//    applicationWillTerminateAction:                                         *
+//----------------------------------------------------------------------------*
+
+- (void) applicationWillTerminateAction: (NSNotification *) inNotification {
+  // NSLog (@"%s", __PRETTY_FUNCTION__) ;
+  // NSLog (@"%d", self.mAllocationStatsWindowVisibleAtLaunch) ;
+  NSUserDefaults * ud = [NSUserDefaults standardUserDefaults] ;
+  [ud
+    setBool:self.mAllocationStatsWindowVisibleAtLaunch
+    forKey:@"PMDebug:allocationStatsWindowVisible"
+  ] ;
+  [ud
+    setInteger:self.mDisplayFilter
+    forKey:@"PMDebug:allocationStatsDisplayFilter"
+  ] ;
+}
+
+//----------------------------------------------------------------------------*
+//    triggerRefreshAllocationStatsDisplay                                    *
+//----------------------------------------------------------------------------*
+
+- (void) triggerRefreshAllocationStatsDisplay {
+  if (! mRefreshStatsHasTriggered) {
+    mRefreshStatsHasTriggered = YES ;
+    [[NSRunLoop currentRunLoop]
+      performSelector:@selector (refreshAllocationStats)
+      target:self
+      argument:nil
+      order:NSUIntegerMax
+      modes:[NSArray arrayWithObject:NSDefaultRunLoopMode]
+    ] ;
+  }
+}
+
+
+//----------------------------------------------------------------------------*
+//    didChangeValueForKey:                                                   *
+//----------------------------------------------------------------------------*
+
+- (void) didChangeValueForKey: (NSString *) inKey {
+  [super didChangeValueForKey:inKey] ;
+  if ([inKey isEqualToString:@"mDisplayFilter"]) {
+    [self triggerRefreshAllocationStatsDisplay] ;
+  }
+}
+
+//----------------------------------------------------------------------------*
+//    pmNoteObjectAllocation:                                                 *
+//----------------------------------------------------------------------------*
+
+- (void) pmNoteObjectAllocation: (NSObject *) inObject {
+  mLiveAllocatedObjectCount ++ ;
+  mLiveTotalObjectCount ++ ;
+  NSString * objectClassName = inObject.className ;
+  //NSLog (@"objectClassName %@", objectClassName) ;
+  [mAllocatedObjectCountByClass addObject:objectClassName] ;
+  [mTotalAllocatedObjectCountByClass addObject:objectClassName] ;
+  [self triggerRefreshAllocationStatsDisplay] ;
+}
+
+//----------------------------------------------------------------------------*
+//    pmNoteObjectDeallocation:                                               *
+//----------------------------------------------------------------------------*
+
+- (void) pmNoteObjectDeallocation: (NSObject *) inObject {
+  mLiveAllocatedObjectCount -- ;
+  NSString * objectClassName = inObject.className ;
+  // NSLog (@"DEALLOC objectClassName %@", objectClassName) ;
+  [mAllocatedObjectCountByClass removeObject:objectClassName] ;
+  [self triggerRefreshAllocationStatsDisplay] ;
+}
+
+//----------------------------------------------------------------------------*
+//    refreshAllocationStats                                                  *
+//----------------------------------------------------------------------------*
+
+- (void) refreshAllocationStats {
+  mRefreshStatsHasTriggered = NO ;
+//---
+  self.mAllocatedObjectCount = mLiveAllocatedObjectCount ;
+  self.mTotalAllocatedObjectCount = mLiveTotalObjectCount ;
+//---
+  NSMutableArray * array = [NSMutableArray new] ;
+  for (NSString * className in [mTotalAllocatedObjectCountByClass.allObjects sortedArrayUsingSelector:@selector(compare:)]) {
+    const NSUInteger currentlyAllocated = [mAllocatedObjectCountByClass countForObject:className] ;
+    if ((currentlyAllocated != 0) || (self.mDisplayFilter == 0)) {
+      [array addObject: [NSDictionary
+        dictionaryWithObjectsAndKeys:
+          className, @"classname",
+          [NSNumber numberWithUnsignedInteger:[mTotalAllocatedObjectCountByClass countForObject:className]], @"allCount",
+          [NSNumber numberWithUnsignedInteger:currentlyAllocated], @"live",
+          nil
+        ]
+      ] ;
+    }
+  }
+//---
+  mAllocationStatsDataSource = array ;
+  [mStatsTableView setDataSource:self] ;
+  [mStatsTableView reloadData] ;
+}
+
+//----------------------------------------------------------------------------*
+//    T A B L E   V I E W    D A T A    S O U R C E                           *
+//----------------------------------------------------------------------------*
+
+- (id) tableView: (NSTableView *) aTableView
+       objectValueForTableColumn: (NSTableColumn *) aTableColumn
+       row: (NSInteger) rowIndex {
+  NSDictionary * theRecord = [mAllocationStatsDataSource objectAtIndex:(NSUInteger) rowIndex];
+  return [theRecord valueForKey:aTableColumn.identifier] ;
+}
+
+//---------------------------------------------------------------------------*
+
+- (NSInteger) numberOfRowsInTableView: (NSTableView *) aTableView {
+  return (NSInteger) [mAllocationStatsDataSource count] ;
+}
+
+//----------------------------------------------------------------------------*
+
+@end
+
