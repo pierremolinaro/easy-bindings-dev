fc351c9229ee571d12d53749cad89ce0694ff6f0
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2012-11-13 18:26:01 +0000

    Ajout exemple 07 — simples classes

---
+++ sample-projects/07-simple-classes/07-simple-classes/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  07-simple-classes
+//
+//  Created by Pierre Molinaro on 13/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PMAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ sample-projects/07-simple-classes/07-simple-classes/PMAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  PMAppDelegate.m
+//  07-simple-classes
+//
+//  Created by Pierre Molinaro on 13/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PMAppDelegate.h"
+
+@implementation PMAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
