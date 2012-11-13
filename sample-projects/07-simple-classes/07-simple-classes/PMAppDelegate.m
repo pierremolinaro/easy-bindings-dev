49d99dff1f5b60691a48d1d44b82eefefada58ca
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2012-11-13 22:12:02 +0000

    Modification key value observing

--- sample-projects/07-simple-classes/07-simple-classes/PMAppDelegate.m
+++ sample-projects/07-simple-classes/07-simple-classes/PMAppDelegate.m
@@ -5,16 +5,47 @@
 //  Created by Pierre Molinaro on 13/11/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+//----------------------------------------------------------------------------*
 
 #import "PMAppDelegate.h"
+#import "MyOtherClass.h"
+
+//----------------------------------------------------------------------------*
 
 @implementation PMAppDelegate
 
 @synthesize window = _window;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-  // Insert code here to initialize your application
+//----------------------------------------------------------------------------*
+
+- (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
 }
+
+//----------------------------------------------------------------------------*
+
+- (void) awakeFromNib {
+  mObject = [[MyOtherClass alloc]
+    initWithMyBool:YES
+    withMyInteger32:0
+    withMyString:@"azerty"
+    withChose:NO
+  ] ;
+//---
+  [mSchmurtzTextField
+    bind:@"value"
+    toObject:mObject
+    withKeyPath:@"schmurtz"
+    options:nil
+  ] ;
+}
+
+//----------------------------------------------------------------------------*
+
+- (IBAction) incrementMyInteger32: (id) inSender {
+  mObject.myInteger32 = mObject.myInteger32 + 1 ;
+  mObject.chose = YES ;
+}
+
+//----------------------------------------------------------------------------*
 
 @end
