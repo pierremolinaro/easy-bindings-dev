d4d96236fb83530ca5b0ce7a6993fec6b1d19da6
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2012-12-23 18:21:48 +0000

    Ajout exploration de document

---
+++ sample-projects/04-document-with-relationships/04-document-with-relationships/PMAbstractPersistentDocument.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------*
+//                                                                           *
+//  File 'PMAbstractPersistentDocument.h'                                    *
+//  Created by Pierre Molinaro on january 23, 2008.                          *
+//                                                                           *
+//---------------------------------------------------------------------------*
+
+#import <Cocoa/Cocoa.h>
+
+//---------------------------------------------------------------------------*
+
+@class PMManagedObject ;
+
+//---------------------------------------------------------------------------*
+
+@interface PMAbstractPersistentDocument : NSDocument {
+  @private BOOL mFetchingAllObjectsDone ;
+  @private NSMutableArray * mUnreachableObjectsForCheckObjectGraph ;
+  @private NSManagedObjectContext * mManagedObjectContext ;
+}
+
+- (NSManagedObjectContext *) managedObjectContext ;
+
+- (IBAction) checkRelationships: (id) inSender ;
+
+- (IBAction) checkObjectGraph: (id) inUnusedSender ;
+
+- (PMManagedObject *) rootObject ;
+
+@end
+
+//---------------------------------------------------------------------------*
+
