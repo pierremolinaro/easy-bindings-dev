e3f920b81ab24fb8a5490290a7e1c71ad990dba7
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2014-06-09 09:18:43 +0000

    Suppression fichiers inutiles

---
+++ -clean-all.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#------------------------------------------------------------------------------*
+# https://docs.python.org/2/library/subprocess.html#module-subprocess
+
+import subprocess
+import sys
+import os
+import shutil
+
+#------------------------------------------------------------------------------*
+
+def deleteDir (inDirName) :
+  scriptDir = os.path.dirname (os.path.abspath (sys.argv [0]))
+  #--- Enumerate directories
+  for root, dirs, files in os.walk (scriptDir) :
+      for aDir in dirs :
+        if aDir == inDirName :
+          fullname = os.path.join (root, inDirName)
+          print "*** Remove " + fullname
+          shutil.rmtree (fullname)
+
+#------------------------------------------------------------------------------*
+
+#--- Get script absolute path
+deleteDir ("objects")
+deleteDir ("debug_objects")
+deleteDir ("DerivedData")
+deleteDir ("build")
+deleteDir ("xcuserdata")
+deleteDir ("project.xcworkspace")
+
+#------------------------------------------------------------------------------*
