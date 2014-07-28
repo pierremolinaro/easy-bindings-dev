3606ab7251df038aad82ea2eba5bbfa01e750537
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2014-07-28 12:15:18 +0000

    Début exempte 5 en swift

---
+++ sample-projects-swift/05-observing-pref-in-document-swift/05-observing-pref-in-document/PMObjectManager.h
@@ -0,0 +1,48 @@
+//
+//  PMObjectManager.h
+//  essai
+//
+//  Created by Pierre Molinaro on 28/06/13.
+//  Copyright (c) 2013 ECN / IRCCyN. All rights reserved.
+//
+//-----------------------------------------------------------------------------*
+
+#import <Foundation/Foundation.h>
+
+//-----------------------------------------------------------------------------*
+
+@class PMManagedObject ;
+
+//-----------------------------------------------------------------------------*
+
+@interface PMObjectManager : NSObject {
+  @private NSUndoManager * mUndoManager ;
+  @private NSMutableSet * mManagedObjectSet ;
+}
+
+- (instancetype) initWithUndoManager: (NSUndoManager *) inUndoManager ;
+
+- (NSUndoManager *) undoManager ;
+
+- (id) newInstanceOfEntity: (Class) inEntityClass ;
+
+- (void) deleteEntity: (PMManagedObject *) inObject ;
+
+- (void) deleteEntities: (NSArray *) inObjects ;
+
+- (NSArray *) allEntitiesKindOfClass: (Class) inClass ;
+
+- (NSArray *) reachableObjectsFromObject: (PMManagedObject *) inRootObject ;
+
+- (NSSet *) uneachableObjectsFromObject: (PMManagedObject *) inRootObject ;
+
+- (id) readFromData: (NSData *) inData withRootEntityClass: (Class) inRootEntityClass ;
+
+- (void) resetBeforeDeletion ;
+
+- (NSData *) dataForSavingFromRootObject: (PMManagedObject *) inRootObject ;
+
+- (NSUInteger) entityCount ;
+@end
+
+//-----------------------------------------------------------------------------*
