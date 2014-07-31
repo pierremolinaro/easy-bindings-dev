add3e6b22cd7c0717f4aa81bb39ff0489c1ab6a6
Pierre Molinaro <pierre@pcmolinaro.name>
2014-07-31 20:52:51 +0000

    Exemple 07 en swift

---
+++ sample-projects-swift/07-simple-classes-swift/07-simple-classes/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  04-document-with-relationships-swift
+//
+//  Created by Pierre Molinaro on 29/06/2014.
+//  Copyright (c) 2014 Pierre Molinaro. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
