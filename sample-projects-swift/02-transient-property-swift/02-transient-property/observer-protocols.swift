d8ab70139375cfacbdb701f9256af986a3672315
pm <pm@db83cac3-9ea6-0410-bf0f-ff9db4435373>
2014-08-06 22:05:17 +0000

    Insertion des observer protocols dans un fichier séparé

---
+++ sample-projects-swift/02-transient-property-swift/02-transient-property/observer-protocols.swift
@@ -0,0 +1,16 @@
+@objc(protocol_preference_2E_Prefs_2E_mFirstName) protocol protocol_preference_2E_Prefs_2E_mFirstName {
+  func preference_2E_Prefs_2E_mFirstName_didChange ()
+}
+
+@objc(protocol_preference_2E_Prefs_2E_mFullName) protocol protocol_preference_2E_Prefs_2E_mFullName {
+  func preference_2E_Prefs_2E_mFullName_didChange ()
+}
+
+@objc(protocol_preference_2E_Prefs_2E_mName) protocol protocol_preference_2E_Prefs_2E_mName {
+  func preference_2E_Prefs_2E_mName_didChange ()
+}
+
+@objc(protocol_preference_2E_Prefs_2E_mUpperCaseFullName) protocol protocol_preference_2E_Prefs_2E_mUpperCaseFullName {
+  func preference_2E_Prefs_2E_mUpperCaseFullName_didChange ()
+}
+
---
+++ sample-projects-swift/04-document-with-relationships-swift/04-document-with-relationships-swift/observer-protocols.swift
@@ -0,0 +1,28 @@
+@objc(protocol_entity_2E_MyRootEntity_2E_mNames) protocol protocol_entity_2E_MyRootEntity_2E_mNames {
+  func entity_2E_MyRootEntity_2E_mNames_didChange ()
+}
+
+@objc(protocol_entity_2E_NameEntity_2E_aValue) protocol protocol_entity_2E_NameEntity_2E_aValue {
+  func entity_2E_NameEntity_2E_aValue_didChange ()
+}
+
+@objc(protocol_entity_2E_NameEntity_2E_mRoot) protocol protocol_entity_2E_NameEntity_2E_mRoot {
+  func entity_2E_NameEntity_2E_mRoot_didChange ()
+}
+
+@objc(protocol_entity_2E_NameEntity_2E_name) protocol protocol_entity_2E_NameEntity_2E_name {
+  func entity_2E_NameEntity_2E_name_didChange ()
+}
+
+@objc(protocol_document_2E_PMDocument_2E_canRemoveString) protocol protocol_document_2E_PMDocument_2E_canRemoveString {
+  func document_2E_PMDocument_2E_canRemoveString_didChange ()
+}
+
+@objc(protocol_document_2E_PMDocument_2E_countItemMessage) protocol protocol_document_2E_PMDocument_2E_countItemMessage {
+  func document_2E_PMDocument_2E_countItemMessage_didChange ()
+}
+
+@objc(protocol_document_2E_PMDocument_2E_nameController) protocol protocol_document_2E_PMDocument_2E_nameController {
+  func document_2E_PMDocument_2E_nameController_didChange ()
+}
+
