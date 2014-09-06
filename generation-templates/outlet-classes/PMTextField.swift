13ae6e6ad6e66f06dfd69a9b5eaa69d76451a3e4
Pierre Molinaro <pierre@pcmolinaro.name>
2014-09-06 10:27:35 +0000

    Suite

--- generation-templates/outlet-classes/PMTextField.swift
+++ generation-templates/outlet-classes/PMTextField.swift
@@ -5,11 +5,66 @@ import Cocoa
 //---------------------------------------------------------------------------------------------------------------------*
 
 @objc(PMTextField) class PMTextField : NSTextField {
+
+  //-------------------------------------------------------------------------------------------------------------------*
+
+  required init (coder: NSCoder!) {
+    super.init (coder:coder)
+    noteObjectAllocation (self)
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------*
+
+  deinit {
+    if mSendContinously {
+      NSNotificationCenter.defaultCenter().removeObserver (self,
+        name: NSControlTextDidChangeNotification,
+        object: self
+      )
+    }
+    noteObjectDeallocation (self)
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------*
+
+  private var mSendContinously : Bool = false
+  
+  //-------------------------------------------------------------------------------------------------------------------*
+
+  func setSendContinously (flag : Bool) {
+    if mSendContinously != flag {
+      mSendContinously = flag
+      if mSendContinously {
+        NSNotificationCenter.defaultCenter().addObserver (self,
+          selector: "continouslySendAction:",
+          name: NSControlTextDidChangeNotification,
+          object: self
+        )
+      }else{
+        NSNotificationCenter.defaultCenter().removeObserver (self,
+          name: NSControlTextDidChangeNotification,
+          object: self
+        )
+      }
+    }
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------*
+
+  func continouslySendAction (notification : NSNotification) {
+    NSApp.sendAction (self.action, to: self.target, from: self)
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------*
+
   var enableFromEnableBinding : Bool = true {
     didSet {
       self.enabled = enableFromEnableBinding & enableFromValueBinding
     }
   }
+
+  //-------------------------------------------------------------------------------------------------------------------*
+
   var enableFromValueBinding : Bool = true {
     didSet {
       self.enabled = enableFromEnableBinding & enableFromValueBinding
