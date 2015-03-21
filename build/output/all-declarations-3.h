70997159cb1951ec266f5c120190cb726ff30479
Pierre Molinaro <pierre@pcmolinaro.name>
2015-03-21 20:39:42 +0000

    Suite

--- build/output/all-declarations-3.h
+++ build/output/all-declarations-3.h
@@ -9,4 +9,17 @@
 
 #include "all-declarations-2.h"
 
+//---------------------------------------------------------------------------------------------------------------------*
+//                                                                                                                     *
+//                      Category method '@observablePropertyAST enterInTransientDependencyGraph'                       *
+//                                                                                                                     *
+//---------------------------------------------------------------------------------------------------------------------*
+
+void categoryMethod_enterInTransientDependencyGraph (const class GALGAS_observablePropertyAST inObject,
+                                                     class GALGAS_string in_inDependencyGraphPrefix,
+                                                     class GALGAS_lstring in_inSlavePropertySignature,
+                                                     class GALGAS_transientDependencyGraph & io_ioTransientDependencyGraph,
+                                                     class C_Compiler * inCompiler
+                                                     COMMA_LOCATION_ARGS) ;
+
 #endif
