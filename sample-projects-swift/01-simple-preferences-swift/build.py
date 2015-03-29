85cd4d4e8f30903b5b98bbab437d2b02b0af388d
Pierre Molinaro <pierre@pcmolinaro.name>
2015-03-29 16:34:55 +0000

    Exemple 4

---
+++ sample-projects-swift/01-simple-preferences-swift/build.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#------------------------------------------------------------------------------*
+
+import subprocess, os, sys, atexit
+
+#------------------------------------------------------------------------------*
+
+def cleanup () :
+  if buildProcess.poll () == None :
+    buildProcess.kill ()
+    print 'Build process killed'
+
+#------------------------------------------------------------------------------*
+
+#--- Register a function for killing build subprocess
+atexit.register (cleanup)
+#------------------- Get script absolute path
+scriptDir = os.path.dirname (os.path.abspath (sys.argv [0]))
+os.chdir (scriptDir)
+#------------------- Build
+buildProcess = subprocess.Popen (["eb", "-v", "simple-preferences.eb"])
+buildProcess.wait ()
+#------------------- Xcode build
+if buildProcess.returncode == 0 :
+  buildProcess = subprocess.Popen (["/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild"])
+  buildProcess.wait ()
+#-------------------
+
+#------------------------------------------------------------------------------*
