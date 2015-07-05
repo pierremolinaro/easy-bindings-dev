7497d82c83de9943aee431776138327578878a76
Pierre Molinaro <pierre@pcmolinaro.name>
2015-07-05 15:02:53 +0000

    Ajout EBTextView

---
+++ generation-templates/outlet-classes/EBTextView.swift
@@ -0,0 +1,100 @@
+import Cocoa
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//   EBTextView
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+@objc(EBTextView) class EBTextView : NSTextView, EBUserClassName {
+
+  //····················································································································
+
+  required init? (coder: NSCoder) {
+    super.init (coder:coder)
+    noteObjectAllocation (self)
+  }
+
+  //····················································································································
+
+/*  override init (frame:NSRect) {
+    super.init (frame:frame)
+    noteObjectAllocation (self)
+  } */
+  
+  //····················································································································
+
+  deinit {
+    noteObjectDeallocation (self)
+  }
+
+  //····················································································································
+  //  value binding
+  //····················································································································
+
+  private var mValueController : Controller_EBTextView_value?
+
+  func bind_value (object:EBReadWriteProperty_String, file:String, line:Int) {
+    mValueController = Controller_EBTextView_value (object:object, outlet:self, file:file, line:line)
+  }
+
+  //····················································································································
+
+  func unbind_value () {
+    mValueController?.unregister ()
+    mValueController = nil
+  }
+
+  //····················································································································
+
+}
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//   Controller Controller_EBTextView_value
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+@objc(Controller_EBTextView_value) final class Controller_EBTextView_value : EBSimpleController, NSTextViewDelegate {
+
+  private let mOutlet: EBTextView
+  private let mObject : EBReadWriteProperty_String
+
+  //····················································································································
+
+  init (object:EBReadWriteProperty_String, outlet : EBTextView, file : String, line : Int) {
+    mObject = object
+    mOutlet = outlet
+    super.init (objects:[object], outlet:outlet)
+    outlet.delegate = self
+    object.addObserver (self, postEvent:true)
+  }
+
+  //····················································································································
+  
+  func unregister () {
+    mObject.removeObserver (self, postEvent:false)
+    mOutlet.delegate = nil
+  }
+
+  //····················································································································
+
+  func textDidChange (notification: NSNotification) {
+    let str = mOutlet.string ?? ""
+    mObject.validateAndSetProp (str, windowForSheet:mOutlet.window)
+  }
+
+  //····················································································································
+
+  override func sendUpdateEvent () {
+    switch mObject.prop {
+    case .noSelection, .multipleSelection :
+      mOutlet.string = ""
+      mOutlet.editable = false
+    case .singleSelection (let propertyValue) :
+      mOutlet.string = propertyValue
+      mOutlet.editable = true
+    }
+  }
+
+  //····················································································································
+
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
