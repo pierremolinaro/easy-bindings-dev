499493c0ab8415050aedb1bca909b62653f05c80
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2018-12-02 19:18:08 +0100

    gitignore

---
+++ build/output/all-declarations-8.h
@@ -0,0 +1,151 @@
+#pragma once
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+#include "all-predefined-types.h"
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+#include "all-declarations-7.h"
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                          Filewrapper 'standard_properties'                                          *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+extern const cDirectoryWrapper gWrapperDirectory_0_standard_5F_properties ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                   Filewrapper template 'standard_properties stub'                                   *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_standard_5F_properties_stub (class C_Compiler * inCompiler
+                                                               COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                              Filewrapper template 'standard_properties scalarProperty'                              *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_standard_5F_properties_scalarProperty (class C_Compiler * inCompiler,
+                                                                         const class GALGAS_string & in_TYPE,
+                                                                         const class GALGAS_bool & in_TRANSIENT
+                                                                         COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                              Filewrapper template 'standard_properties classProperty'                               *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_standard_5F_properties_classProperty (class C_Compiler * inCompiler,
+                                                                        const class GALGAS_string & in_CLASS_5F_NAME,
+                                                                        const class GALGAS_bool & in_COMPARABLE,
+                                                                        const class GALGAS_bool & in_TRANSIENT
+                                                                        COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                       Routine 'generateXcodeProjectInternal'                                        *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+void routine_generateXcodeProjectInternal (const class GALGAS_string constinArgument0,
+                                           const class GALGAS_string constinArgument1,
+                                           const class GALGAS_stringset constinArgument2,
+                                           const class GALGAS_string constinArgument3,
+                                           class C_Compiler * inCompiler
+                                           COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                         Filewrapper 'additionalXcodeFiles'                                          *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+extern const cDirectoryWrapper gWrapperDirectory_0_additionalXcodeFiles ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                Filewrapper template 'additionalXcodeFiles main_xib'                                 *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_additionalXcodeFiles_main_5F_xib (class C_Compiler * inCompiler,
+                                                                    const class GALGAS_string & in_PROJECT_5F_NAME
+                                                                    COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                               Filewrapper template 'additionalXcodeFiles info_plist'                                *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_additionalXcodeFiles_info_5F_plist (class C_Compiler * inCompiler,
+                                                                      const class GALGAS_string & in_PROJECT_5F_NAME,
+                                                                      const class GALGAS_string & in_APPLICATION_5F_BUNDLE_5F_BASE,
+                                                                      const class GALGAS_string & in_VERSION_5F_STRING,
+                                                                      const class GALGAS_stringset & in_HANDLED_5F_EXTENSION_5F_SET
+                                                                      COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                              Filewrapper template 'additionalXcodeFiles build_command'                              *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_additionalXcodeFiles_build_5F_command (class C_Compiler * inCompiler,
+                                                                         const class GALGAS_string & in_PROJECT_5F_NAME
+                                                                         COMMA_LOCATION_ARGS) ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                               Filewrapper 'testFiles'                                               *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+extern const cDirectoryWrapper gWrapperDirectory_0_testFiles ;
+
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+//                                                                                                                     *
+//                                      Filewrapper template 'testFiles test_py'                                       *
+//                                                                                                                     *
+//—————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
+
+GALGAS_string filewrapperTemplate_testFiles_test_5F_py (class C_Compiler * inCompiler,
+                                                        const class GALGAS_string & in_BUNDLE_5F_IDENTIFIER,
+                                                        const class GALGAS_string & in_PROJECT_5F_NAME,
+                                                        const class GALGAS_stringlist & in_OUTLET_5F_IDENTIFIER_5F_LIST
+                                                        COMMA_LOCATION_ARGS) ;
+
