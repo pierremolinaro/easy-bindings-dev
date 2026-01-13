22c56fdf1d0a5bd20460b9a794b549d2f4baab0e
pierremolinaro <pierre@pcmolinaro.name>
2026-01-13 21:03:05 +0100

    Suite

--- +easy-bindings+ggsproject+BUILD/output/gui-swiftui-cocoa.swift
+++ +easy-bindings+ggsproject+BUILD/output/gui-swiftui-cocoa.swift
@@ -14,46 +14,55 @@ func indexingDescriptorDictionary () -> [String : String] {
 }
 
 //--------------------------------------------------------------------------------------------------
-//   Global functions
+//   Scanner for a given extension
 //--------------------------------------------------------------------------------------------------
 
 @MainActor func scannerFor (extension inExtension : String) -> SWIFT_Scanner? {
   var result : SWIFT_Scanner? = nil
-  if inExtension == "eb" {
+  if inExtension.compare ("eb", options: .caseInsensitive) == .orderedSame {
     result = ScannerFor_easyBindings_lexique ()
   }
   return result
 }
 
 //--------------------------------------------------------------------------------------------------
-
-/* @MainActor func tokenizers () -> [any SWIFT_Tokenizer_Protocol] {
-  return [
-    SettingViewFor_easyBindings_lexique ()
-  ]
-} */
-
+// Setting View
 //--------------------------------------------------------------------------------------------------
 
 struct SettingsView : View {
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  enum SidebarItem {
+    case commandLineOptions
+    case easyBindings_lexique
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  @State private var mSelection : SidebarItem = .commandLineOptions
+
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   @ViewBuilder var body : some View {
-    TabView {
-      SettingViewFor_easyBindings_lexique ().tabItem { Text ("Source") }
+    NavigationSplitView {
+      List(selection: self.$mSelection) {
+        Text ("Options").tag (SidebarItem.commandLineOptions)
+
+        Text ("Source").tag (SidebarItem.easyBindings_lexique)
+      }
+      .toolbar (removing: .sidebarToggle)
+    } detail: {
+      switch self.mSelection {
+        case .commandLineOptions : OptionView ()
+        case .easyBindings_lexique : SettingViewFor_easyBindings_lexique ()
+      }
     }
   }
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 }
-
-
-//--------------------------------------------------------------------------------------------------
-
-/* func buildRunOption () -> String {
-  return ""
-} */
 
 //--------------------------------------------------------------------------------------------------
 
